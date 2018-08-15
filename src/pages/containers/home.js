ded1fd1518ce9166b59ecf3d0930542b73152f48
Hector Mrn <hector.mrn55@gmail.com>
2018-08-15 15:55:45 -0500

    Start project setup

---
+++ src/pages/containers/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//import HomeLayout from '../components/home-layout';
+
+class Home extends Component {
+  
+  render() {
+    return (
+      <div>E-SocialSeeker App</div>
+    )
+  }
+}
+
+export default Home
